598f5363ea1cb509194ea950a2015d68dbbb5a64
Phuc Le <lelocminhphuc@gmail.com>
2021-03-27 18:51:50 +0100

    Commit Chapter 1
    Introduction and HelloWorld program in Perl

---
+++ 1-Introduction/helloworld.pl
@@ -0,0 +1,41 @@
+use strict;
+use warnings;
+
+print "Hello world\n"; # this line prints Hello world and ends with new line
+
+=pod
+
+=head1 Hello World exercise document
+
+=over 4
+
+=item * 
+Type 'perl -v' to get documentation of Perl.
+
+=item *
+'perl -V' prints out detail documentaiton of Perl.
+
+=item *
+'perl -e "print 42"' prints out 42 (no new line) directly from cmd.
+
+=item *
+'perl -E "say 42"' prints out 42 (with new line) directly from cmd.
+
+=item *
+'perldoc perl' prints out how you can run perl and so on, press q to quit.
+
+=item *
+'perldoc perlsyn' prints out perl syntax.
+
+=item *
+'aptitude search perl-doc' install perl-doc.
+
+=item *
+ 'pod2html helloworld.pl > helloworld.html' then
+ 'lynx helloworl.html' (available in linux only)
+
+view the perldoc as html file.
+
+=back
+
+=cut
